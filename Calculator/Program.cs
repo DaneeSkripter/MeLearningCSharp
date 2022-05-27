ca624df987944d99640b9bb50daf34c189eab27a
DaneeSkripter <63862773+DaneeSkripter@users.noreply.github.com>
2022-05-27 15:43:41 +0200

    loop added

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1 // Note: actual namespace depends on the project name.
             Console.Write("Enter another number: ");
             double num2 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine(num1 + num2);
+            Main();
         }
     }
 }
