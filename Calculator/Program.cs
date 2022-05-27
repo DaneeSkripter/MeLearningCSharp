27151e8f0242c050b99dd702a22f1329d589dea9
DaneeSkripter <63862773+DaneeSkripter@users.noreply.github.com>
2022-05-27 16:05:09 +0200

    More functions

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,16 +1,32 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp1 // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.Write("Enter number: ");
             double num1 = Convert.ToDouble(Console.ReadLine());
             Console.Write("Enter another number: ");
             double num2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(num1 + num2);
+            Console.Write("Enter action (+, -, :, x): ");
+            string action = Console.ReadLine();
+            if (action == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            } else if (action == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            } else if (action == ":")
+            {
+                Console.WriteLine(num1 / num2);
+            } else if (action == "x")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            System.Threading.Thread.Sleep(3000);
+            Console.Clear();
             Main();
         }
     }
