cc5bc3b53d426bb93521e7a9beef51c1ce74d79f
DaneeSkripter <63862773+DaneeSkripter@users.noreply.github.com>
2022-05-27 15:35:09 +0200

    📜 Calculator in C#

---
+++ Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
